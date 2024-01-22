826a7b4ab8e826973df25502d77db659ac440f4d
TanishqaThakur0310 <87258215+TanishqaThakur0310@users.noreply.github.com>
2024-01-22 12:58:48 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDsZtuJrnFLUjeOlPHoMJn-IAxL-CJUpjs",
+  authDomain: "e-ride-4883b.firebaseapp.com",
+  databaseURL: "https://e-ride-4883b-default-rtdb.firebaseio.com",
+  projectId: "e-ride-4883b",
+  storageBucket: "e-ride-4883b.appspot.com",
+  messagingSenderId: "984767375097",
+  appId: "1:984767375097:web:93443005965cdeb9b46428"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
